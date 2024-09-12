87e88652dd122d2210e7a09e8581935e138f9fea
Riccardo Benedetti <github@benedetti.dev>
2024-09-12 10:09:33 +0200

    feat: restored account theme

--- src/account/KcPage.tsx
+++ src/account/KcPage.tsx
@@ -1,11 +1,58 @@
 import { lazy } from "react";
 import { KcAccountUiLoader } from "@keycloakify/keycloak-account-ui";
 import type { KcContext } from "./KcContext";
+import './main.css';
 
 const KcAccountUi = lazy(() => import("@keycloakify/keycloak-account-ui/KcAccountUi"));
 
 export default function KcPage(props: { kcContext: KcContext }) {
     const { kcContext } = props;
 
-    return <KcAccountUiLoader kcContext={kcContext} KcAccountUi={KcAccountUi} />;
+    return <KcAccountUiLoader
+        kcContext={kcContext}
+        KcAccountUi={KcAccountUi}
+        content={[
+            {
+                label: "personalInfo",
+                path: ""
+            },
+            {
+                label: "accountSecurity",
+                children: [
+                    {
+                        label: "signingIn",
+                        path: "account-security/signing-in"
+                    },
+                    {
+                        label: "deviceActivity",
+                        path: "account-security/device-activity"
+                    },
+                    {
+                        label: "linkedAccounts",
+                        path: "account-security/linked-accounts",
+                        isVisible: "isLinkedAccountsEnabled"
+                    }
+                ]
+            },
+            {
+                label: "Torna a evolus",
+                path: "applications"
+            },
+            {
+                label: "groups",
+                path: "groups",
+                isVisible: "isViewGroupsEnabled"
+            },
+            {
+                label: "resources",
+                path: "resources",
+                isVisible: "isMyResourcesEnabled"
+            },
+            {
+                label: "oid4vci",
+                path: "oid4vci",
+                isVisible: "isOid4VciEnabled"
+            }
+        ]}
+    />;
 }
